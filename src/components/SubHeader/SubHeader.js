ea25a51b09cec30ca6e5ed77f1a5b254e72dd315
Ovijitnath <ovijitnathgn@gmail.com>
2022-10-12 22:55:49 +0600

    footer issue solved

--- src/components/SubHeader/SubHeader.js
+++ src/components/SubHeader/SubHeader.js
@@ -1,12 +1,5 @@
 import React from 'react';
 
-// const SubHeader = () => {
-//     return (
-//         <div>
-//             <h1>This is sub header</h1>
-//         </div>
-//     );
-// };
 export const SubHeader = () => {
     return (
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
