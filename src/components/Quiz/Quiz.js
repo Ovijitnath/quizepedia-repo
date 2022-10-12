5bfa085c3044cc257576f640bf3c1aa56dbfd844
Ovijitnath <ovijitnathgn@gmail.com>
2022-10-12 22:52:37 +0600

    correct wron issue solved

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -5,10 +5,11 @@ import { faEye } from '@fortawesome/free-solid-svg-icons'
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Option from '../Option';
+
 
 const Quiz = ({ quiz }) => {
-    const { id, question, options, correctAnswer } = quiz
+    const { id, question, options, correctAnswer } = quiz;
+
     const notify = () => toast(correctAnswer);
 
     return (
@@ -18,12 +19,26 @@ const Quiz = ({ quiz }) => {
                 <h1 className='text-cyan-900'>{question.slice(3, -4)}</h1>
 
             </div>
-            <div className='fw-semibold'>
-                {
-                    options.map(option => <Option key={option.id} option={option}></Option>
-                    )
-                }
+            <div className="radio-buttons">
+                {options.map(option => <div >  <input
+                    type="radio"
+                    name="radio"
+                    onChange={() => {
+                        if (correctAnswer === option) {
+
+                            toast("Your Answer is Right!");
+                        } else {
+                            toast("Sorry! Your Answer is wrong!");
+                        }
+                    }}
+                    className="radio checked:bg-red-500"
+                /> {option} </div>)}
+                <ToastContainer />
+
+
+
             </div>
+
             <div className='pt-4 text-lime-700'>
                 Correct Answer: <FontAwesomeIcon icon={faEye} onClick={notify} />
                 <ToastContainer />
